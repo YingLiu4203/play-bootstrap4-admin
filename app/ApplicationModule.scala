8fbf73f0a924d2df26d968d9206b62fe5c5e29c5
yingliu <yingliu4203@gmail.com>
2019-11-05 14:01:45 -0800

    add all files

--- app/ApplicationModule.scala
+++ app/ApplicationModule.scala
@@ -1,6 +1,6 @@
 import play.api.i18n.Langs
 import play.api.mvc.ControllerComponents
-import controllers.{Home, UIElements, Utilities}
+import controllers.{Home, UIElements, Utilities, Pages, ChartTable}
 
 trait ApplicationModule {
 
@@ -9,6 +9,8 @@ trait ApplicationModule {
   lazy val home = wire[Home]
   lazy val uiElement = wire[UIElements]
   lazy val utilities = wire[Utilities]
+  lazy val pages = wire[Pages]
+  lazy val chartTable = wire[ChartTable]
 
   def langs: Langs
 
