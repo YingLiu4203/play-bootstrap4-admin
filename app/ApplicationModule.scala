2351ce6160a57bef475813af67c47241bc5f4968
yingliu <yingliu4203@gmail.com>
2019-11-05 12:43:25 -0800

    Add utility pages

--- app/ApplicationModule.scala
+++ app/ApplicationModule.scala
@@ -1,13 +1,14 @@
-
-import controllers.Home
 import play.api.i18n.Langs
 import play.api.mvc.ControllerComponents
+import controllers.{Home, UIElements, Utilities}
 
 trait ApplicationModule {
 
   import com.softwaremill.macwire._
 
   lazy val home = wire[Home]
+  lazy val uiElement = wire[UIElements]
+  lazy val utilities = wire[Utilities]
 
   def langs: Langs
 
--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import play.api.mvc._
-import play.twirl.api.Html
 
 class Home(cc: ControllerComponents) extends AbstractController(cc) {
   def index = Action {
